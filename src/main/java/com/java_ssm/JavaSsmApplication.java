80b7d97006259cfd84402dc8d8b113dafac19ae6
dillonl <dillonl49@163.com>
2022-05-27 17:08:11 +0800

    delete: # 😂 删除无用的文件

--- src/main/java/com/java_ssm/JavaSsmApplication.java
+++ src/main/java/com/java_ssm/JavaSsmApplication.java
@@ -4,13 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.java_ssm.mapper")
 public class JavaSsmApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JavaSsmApplication.class, args);
     }
