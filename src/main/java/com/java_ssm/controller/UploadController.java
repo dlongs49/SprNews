0fd68e7818ee3e06169b4ed4cc9c1416d10fd21e
dillonl <dillonl49@163.com>
2022-06-17 00:18:35 +0800

    fix:#  ❤️增加上传图片功能

--- src/main/java/com/java_ssm/controller/UploadController.java
+++ src/main/java/com/java_ssm/controller/UploadController.java
@@ -7,16 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class UploadController {
     @PostMapping("/upload")
     public String Upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
         Upload upload = new Upload();
-        String fileName = "00";
-        upload.setFile(file, request,"uploadImg");
-//        if (upload.setFile(file, request,"uploadImg")) {
-//            fileName = upload.getUploadPath();
-//        }
+        String fileName = "";
+        fileName = upload.setFile(file, request, "uploadImg");
         return fileName;
     }
 }
--- src/main/java/com/java_ssm/entity/Upload.java
+++ src/main/java/com/java_ssm/entity/Upload.java
@@ -1,8 +1,14 @@
 package com.java_ssm.entity;
 
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
 
 public class Upload {
     private String name;
@@ -10,12 +16,33 @@ public class Upload {
     private String path;
     private long size;
 
-    public boolean setFile(MultipartFile attach, HttpServletRequest request, String uploader) {
-        String location = "D:\\java\\java_ssm\\src\\main\\resources\\static\\upload";
-        System.out.println(attach);
-        return true;
-    }
+    public String setFile(MultipartFile file, HttpServletRequest request, String uploader) {
+        String location = System.getProperty("user.dir")+"\\src\\main\\resources\\static\\upload\\";
+        String OriginalFilename  = file.getOriginalFilename(); // 文件名称
+        String suffixName = OriginalFilename.substring(OriginalFilename.lastIndexOf('.'));
+        String fileName = UUID.randomUUID()+suffixName;
+        File files = new File(location+fileName);
+        System.out.println(location+fileName);
+        try {
+            file.transferTo(files);
+            return "\\upload\\"+fileName;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "file upload error";
+        }
 
+    }
+    // 暂无使用
+    public class  FileObj{
+        private String path;
+        public String getPath(){
+            return path;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+    }
     public String getUploadPath() {
         return path;
     }
