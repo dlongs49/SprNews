4dd114bfebe984f557b44c4b7ad138954d67d7d4
dillonl <dillonl49@163.com>
2022-11-06 23:08:06 +0800

    fix:# :bug: 修改上传图片无法预览bug

---
+++ src/main/java/com/java_ssm/config/MvcConfig.java
@@ -0,0 +1,13 @@
+package com.java_ssm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:" + System.getProperty("user.dir") + "/src/main/resources/static/upload/");
+    }
+}
